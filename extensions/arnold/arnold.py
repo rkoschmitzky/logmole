4a9fcc5bdf7c55d513817466b0940b273df223e0
rico <contact@ricokoschmitzky.com>
2018-03-04 17:29:27 +0100

    initial arnold extension

---
+++ extensions/arnold/arnold.py
@@ -0,0 +1,16 @@
+from src.containers import LogContainer
+
+
+class ArnoldTimeContainer(LogContainer):
+    pattern = ".*log\sstarted.*(?P<start_time>(\d{2}\:?){3})"
+    representative = "times"
+
+
+class ArnoldHostContainer(LogContainer):
+    pattern = ".*host\s+application:.*(?P<name>(Maya|Katana|Houdini))\s+(?P<host_version>.*$)"
+    representative = "host"
+
+
+class ArnoldLogContainer(LogContainer):
+    pattern = ".*\|\sArnold\s(?P<version>(\d\.?){4})"
+    sub_containers = [ArnoldHostContainer, ArnoldTimeContainer]
