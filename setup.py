529be7623b434e52d184457daa60810aa87a0fd7
rico <contact@ricokoschmitzky.com>
2018-04-19 13:27:54 +0200

    version bump

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ classifiers = [
 
 setup(
     name="logmole",
-    version="0.7.2",
+    version="0.7.3",
     author="Rico Koschmitzky",
     author_email="contact@ricokoschmitzky.com",
     classifiers=classifiers,
