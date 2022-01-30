4a3d01374e56bdce500830bf7dec88637eb8ce30
Rico Koschmitzky <contact@ricokoschmitzky.com>
2022-01-30 14:45:30 +0100

    bump requests and urllib3 to avoid vulnarability
    ref #1

--- setup.py
+++ setup.py
@@ -13,6 +13,8 @@ classifiers = [
     "Programming Language :: Python :: 3",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10"
     "Topic :: Software Development :: Libraries :: Python Modules",
     "Topic :: Utilities",
     "Topic :: System :: Logging",
